d69d6b649824229c59cb54263651062a84740960
BettyET <bettina.wyss@gmx.ch>
2015-10-01 15:49:55 +0200

    critical section component added

--- RemoteControl/Sources/app.c
+++ RemoteControl/Sources/app.c
@@ -7,6 +7,7 @@
 #include "app.h"
 #include "LedDriver.h"
 #include "Wait1.h"
+#include "CS1.c"
 
 
 void APP_run(void)
@@ -14,9 +15,12 @@ void APP_run(void)
 	Led_Init(all);
 	while(1)
 	{
+		CS1_CriticalVariable();
+		CS1_EnterCritical();
 		Led_On(all);
 		WAIT1_Waitms(700);
 		Led_Off(all);
 		WAIT1_Waitms(700);
+		CS1_ExitCritical();
 	}
 }
