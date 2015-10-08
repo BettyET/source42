087b879b217b984b9497fd28cbc632a5793ad981
BettyET <bettina.wyss@gmx.ch>
2015-10-08 15:52:37 +0200

    LocalEvent.h added

---
+++ RemoteControl/Sources/LocalEvent.h
@@ -0,0 +1,15 @@
+/*
+ * LocalEvent.h
+ *
+ *  Created on: 08.10.2015
+ *      Author: User
+ */
+
+#ifndef SOURCES_LOCALEVENT_H_
+#define SOURCES_LOCALEVENT_H_
+
+
+
+
+
+#endif /* SOURCES_LOCALEVENT_H_ */
