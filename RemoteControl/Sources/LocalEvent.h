43ccd5a3edd37f79407bcb8f0c79a997916e45d2
BettyET <bettina.wyss@gmx.ch>
2015-10-08 16:01:26 +0200

    enum events added

--- RemoteControl/Sources/LocalEvent.h
+++ RemoteControl/Sources/LocalEvent.h
@@ -8,7 +8,12 @@
 #ifndef SOURCES_LOCALEVENT_H_
 #define SOURCES_LOCALEVENT_H_
 
-
+typedef enum EVNT_Handle {
+  EVNT_STARTUP,            /*!< System startup Event */
+  EVENT_LED_HEARTBEAT,     /*!< LED heartbeat */
+  EVENT_BUTTON_PRESSED,    /*!< Button pressed */
+  EVNT_NOF_EVENTS          /*!< Must be last one! */
+} EVNT_Handle;
 
 
 
