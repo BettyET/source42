4ec5d4df32ddc4bd7328eea8d48cf46f1c90bd97
Tayac <amenirdis@wolke7.net>
2015-10-08 16:01:25 +0200

    Local Event handling

---
+++ Sumo/Sources/LocalEvent.h
@@ -0,0 +1,20 @@
+/*
+ * LocalEvent.h
+ *
+ *  Created on: 08.10.2015
+ *      Author: Mikey
+ */
+
+#ifndef SOURCES_LOCALEVENT_H_
+#define SOURCES_LOCALEVENT_H_
+
+
+typedef enum EVNT_Handle {
+  EVNT_STARTUP,            /*!< System startup Event */
+  EVENT_LED_HEARTBEAT,     /*!< LED heartbeat */
+  /*!< \todo Your events here */
+  EVNT_NOF_EVENTS       /*!< Must be last one! */
+} EVNT_Handle;
+
+
+#endif /* SOURCES_LOCALEVENT_H_ */
