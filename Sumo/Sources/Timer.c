d567ef9f12339ec77c98bd4269277fc453862f04
Tayac <amenirdis@wolke7.net>
2015-10-09 14:52:15 +0200

    Set Clock settings, added Timer.c and Timer.h files

---
+++ Sumo/Sources/Timer.c
@@ -0,0 +1,24 @@
+/*
+ * Timer.c
+ *
+ *  Created on: 09.10.2015
+ *      Author: Mikey
+ */
+
+#include "Timer.h"
+
+void TMR_OnInterrupt(void) {
+  /* this one gets called from an interrupt!!!! */
+  /*! \todo Add code for a blinking LED here */
+
+}
+
+void TMR_Init(void) {
+  /* nothing needed right now */
+}
+
+void TMR_Deinit(void) {
+  /* nothing needed right now */
+}
+
+
