28a6b5e6a0bab18304df8ffce2f1de3413c43de3
Tayac <amenirdis@wolke7.net>
2015-10-09 16:29:23 +0200

    Timer LED blinken implementiert

--- Sumo/Sources/Timer.c
+++ Sumo/Sources/Timer.c
@@ -8,20 +8,27 @@
 #include "Timer.h"
 #include "Event.h"
 
+#define BLINKING_DURATION_MS 1000
+
 void TMR_OnInterrupt(void) {
-  /* this one gets called from an interrupt!!!! */
-  /*! \todo Add code for a blinking LED here */
 
-	EVNT_SetEvent(EVENT_LED_HEARTBEAT);
+	static int count = 0;
 
+	count++;
+	if(count==(BLINKING_DURATION_MS/TMR_TICK_MS)){
+		count = 0;
+		EVNT_SetEvent(EVENT_LED_HEARTBEAT);
+	}
 }
 
 void TMR_Init(void) {
+
   /* nothing needed right now */
 }
 
 void TMR_Deinit(void) {
   /* nothing needed right now */
+
 }
 
 
