098b9e6127df9e71009896091436a5ccf9d81bf3
ystuder <omega_3@gmx.ch>
2015-10-09 14:52:54 +0200

    Event ist nun Reentrant

--- Common/Event.c
+++ Common/Event.c
@@ -10,6 +10,7 @@
  */
 
 #include "Event.h" /* our own interface */
+#include "CS1.h"
 
 typedef uint8_t EVNT_MemUnit; /*!< memory unit used to store events flags */
 #define EVNT_MEM_UNIT_NOF_BITS  (sizeof(EVNT_MemUnit)*8)
@@ -31,48 +32,62 @@ void EVNT_SetEvent(EVNT_Handle event) {
 }
 
 void EVNT_ClearEvent(EVNT_Handle event) {
-  CLR_EVENT(event);
+	CS1_EnterCritical();
+	CLR_EVENT(event);
+	CS1_ExitCritical();
 }
 
 bool EVNT_EventIsSet(EVNT_Handle event) {
-  return GET_EVENT(event);
+	CS1_EnterCritical();
+	bool tmp = GET_EVENT(event);
+	CS1_ExitCritical();
+	return tmp;
 }
 
 bool EVNT_EventIsSetAutoClear(EVNT_Handle event) {
-  bool res;
-
-  res = GET_EVENT(event);
-  if (res) {
-    CLR_EVENT(event); /* automatically clear event */
-  }
-  return res;
+	bool res;
+	CS1_EnterCritical();
+	res = GET_EVENT(event);
+	if (res) {
+		CLR_EVENT(event); /* automatically clear event */
+	}
+	CS1_ExitCritical();
+	return res;
 }
 
 void EVNT_HandleEvent(void (*callback)(EVNT_Handle)) {
   /* Handle the one with the highest priority. Zero is the event with the highest priority. */
-   EVNT_Handle event;
-   for (event=(EVNT_Handle)0; event<EVNT_NOF_EVENTS; event++) { /* does a test on every event */
-     if (GET_EVENT(event)) { /* event present? */
-       CLR_EVENT(event); /* clear event */
-       break; /* get out of loop */
-     }
-   }
-   if (event != EVNT_NOF_EVENTS) {
-     callback(event);
-     /* Note: if the callback sets the event, we will get out of the loop.
-      * We will catch it by the next iteration.
-      */
-   }
+	EVNT_Handle event;
+	for (event=(EVNT_Handle)0; event<EVNT_NOF_EVENTS; event++)
+	{ /* does a test on every event */
+		CS1_EnterCritical();
+		if (GET_EVENT(event))
+		{ /* event present? */
+			CLR_EVENT(event); /* clear event */
+			CS1_ExitCritical();
+			break; /* get out of loop */
+		}
+		CS1_ExitCritical();
+	}
+	if (event != EVNT_NOF_EVENTS)
+	{
+		callback(event);
+		/* Note: if the callback sets the event, we will get out of the loop.
+		* We will catch it by the next iteration.
+		*/
+	}
 }
 
-void EVNT_Init(void) {
-  uint8_t i;
+void EVNT_Init(void)
+{
+	uint8_t i;
 
-  i = 0;
-  do {
-    EVNT_Events[i] = 0; /* initialize data structure */
-    i++;
-  } while(i<sizeof(EVNT_Events)/sizeof(EVNT_Events[0]));
+	i = 0;
+	do
+	{
+		EVNT_Events[i] = 0; /* initialize data structure */
+		i++;
+	} while(i<sizeof(EVNT_Events)/sizeof(EVNT_Events[0]));
 }
 
 void EVNT_Deinit(void) {
