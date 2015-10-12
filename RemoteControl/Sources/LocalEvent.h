daa99f74e893acb15d6cc2cbfdc45e1b69aa5a8c
BettyET <bettina.wyss@gmx.ch>
2015-10-12 10:44:26 +0200

    added events for all buttons

--- RemoteControl/Sources/LocalEvent.h
+++ RemoteControl/Sources/LocalEvent.h
@@ -12,6 +12,12 @@ typedef enum EVNT_Handle {
   EVNT_STARTUP,            /*!< System startup Event */
   EVENT_LED_HEARTBEAT,     /*!< LED heartbeat */
   EVENT_BUTTON_1_PRESSED,    /*!< Button pressed */
+  EVENT_BUTTON_2_PRESSED,
+  EVENT_BUTTON_3_PRESSED,
+  EVENT_BUTTON_4_PRESSED,
+  EVENT_BUTTON_5_PRESSED,
+  EVENT_BUTTON_6_PRESSED,
+  EVENT_BUTTON_7_PRESSED,
   EVNT_NOF_EVENTS          /*!< Must be last one! */
 } EVNT_Handle;
 
