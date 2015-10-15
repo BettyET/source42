8fd7213829b97c74973c529a3601c127eb6e65b3
BettyET <bettina.wyss@gmx.ch>
2015-10-15 15:10:24 +0200

    SCI added

--- RemoteControl/Sources/app.c
+++ RemoteControl/Sources/app.c
@@ -10,6 +10,8 @@
 #include "CS1.c"
 #include "Event.h"
 #include "LED1.h"
+#include "CLS1.h"
+#include "AS1.h"
 
 void myEvents(EVNT_Handle event);
 
@@ -17,10 +19,12 @@ void APP_run(void)
 {
 	Led_Init(all);
 	EVNT_Init();
-
+	CLS1_Init();
 	while(1)
 	{
+		CLS1_SendStr("Hello!\r\n", CLS1_GetStdio()->stdOut);
 		EVNT_HandleEvent(myEvents);
+		WAIT1_Waitms(500);
 	}
 }
 
