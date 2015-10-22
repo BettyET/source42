a45a9571a5cc8d4961b701b3500456d3af1a101b
BettyET <bettina.wyss@gmx.ch>
2015-10-22 16:14:38 +0200

    removed LocalTimer.h and added TMR_TICK_MS in platform.h

--- RemoteControl/Sources/Platform.h
+++ RemoteControl/Sources/Platform.h
@@ -9,8 +9,11 @@
 #define SOURCES_PLATFORM_H_
 
 #define PL_CONFIG_HAS_KBI 0
+
+/* Generate heartbeat with events or trigger? */
 #define HARTBEAT_EVENT 0
 #define HARTBEAT_TRIGGER 1
 
+#define TMR_TICK_MS  1
 
 #endif /* SOURCES_PLATFORM_H_ */
