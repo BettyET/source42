996863a8fcf0e3fd6dbd174257d11b527bb5fa0d
BettyET <bettina.wyss@gmx.ch>
2015-10-30 16:01:53 +0100

    Parser für App hinzugefügt

--- RemoteControl/Sources/app.h
+++ RemoteControl/Sources/app.h
@@ -8,9 +8,11 @@
 #ifndef SOURCES_APP_H_
 #define SOURCES_APP_H_
 
+#include "CLS1.h"
+
 
 void APP_run(void);
 void APP_Init(void);
-
+uint8_t APP_ParseCommand(const unsigned char *cmd, bool *handled, const CLS1_StdIOType *io);
 
 #endif /* SOURCES_APP_H_ */
